e5682753d6e1e7cdd93feb7874205557db60a450
Rory Trunkhill <rtrunkhill@gmail.com>
2017-09-30 19:53:54 +0000

    what's wrong w/ auth?

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'json'
 
 class Kele
     include HTTParty
@@ -6,8 +7,14 @@ class Kele
     
     def initialize(email, pw)
         @auth = { email: email, password: pw }
-        response = self.class.post('https://www.bloc.io/api/v1/sessions', body: @auth)
+        response = self.class.post('/sessions', body: @auth)
         raise 'That didnt work at all' if !@auth
+        @auth_token = response['auth_token']
+    end
+    
+    def get_me
+        response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
+        JSON.parse(response.body)
     end
 end
 
