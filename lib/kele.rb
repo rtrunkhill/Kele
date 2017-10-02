572f23a24e80a925c58e25b8f3677318e7793fa1
Rory Trunkhill <rtrunkhill@gmail.com>
2017-10-02 23:43:24 +0000

    cp4 staight to the master!

--- lib/kele.rb
+++ lib/kele.rb
@@ -16,6 +16,19 @@ class Kele
         response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
         JSON.parse(response.body)
     end
+    
+    def get_mentor_availability(mentor_id)
+        # mentor_id"=>2362517
+        response = self.class.get("/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token }).to_a
+        availability = []
+        response.each do |meet|
+            if meet["booked"] ==  nil
+                availability << meet
+            end
+        end
+        puts availability
+    end
+    
 end
 
 # response =  self.class.post('https://www.bloc.io/api/v1/sessions', body: {'email': email, 'password': pw } )
