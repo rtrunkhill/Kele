4f27af770fad42e8bfa4ba9df7de9086ef405e4e
Rory Trunkhill <rtrunkhill@gmail.com>
2017-10-03 00:38:07 +0000

    cp5 complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,8 +1,10 @@
 require 'httparty'
 require 'json'
+require './lib/roadmap.rb'
 
 class Kele
     include HTTParty
+    include Roadmap
     base_uri 'https://www.bloc.io/api/v1'
     
     def initialize(email, pw)
@@ -14,7 +16,7 @@ class Kele
     
     def get_me
         response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
-        JSON.parse(response.body)
+        @me = JSON.parse(response.body)
     end
     
     def get_mentor_availability(mentor_id)
@@ -28,6 +30,17 @@ class Kele
         end
         puts availability
     end
+    
+    # def get_roadmap(roadmap_id)
+    #     response = self.class.get("/roadmaps/#{roadmap_id}", headers: { "authorization" => @auth_token })
+    #     @roadmap = JSON.parse(response.body)
+    # end
+    
+    # def get_checkpoint(cp_id)
+    #     response = self.class.get("/checkpoints/#{cp_id}", headers: { "authorization" => @auth_token })
+    #     @checkpoints = JSON.parse(response.body)
+    # end
+        
     
 end
 
